1f85da60fefa2f3219de886f348a73b1f580973d
foolfish <tangsiyu24s@ict.ac.cn>
2025-11-19 19:56:51 +0800

    mdbook upgraded to v0.5.0.

---
+++ target/debug/deps/guide_helper-1bdfd144495ab089.d
@@ -0,0 +1,7 @@
+D:\Desktop\Roux\Roux-Beginners\target\debug\deps\guide_helper-1bdfd144495ab089.d: guide-helper\src\lib.rs
+
+D:\Desktop\Roux\Roux-Beginners\target\debug\deps\libguide_helper-1bdfd144495ab089.rlib: guide-helper\src\lib.rs
+
+D:\Desktop\Roux\Roux-Beginners\target\debug\deps\libguide_helper-1bdfd144495ab089.rmeta: guide-helper\src\lib.rs
+
+guide-helper\src\lib.rs:
